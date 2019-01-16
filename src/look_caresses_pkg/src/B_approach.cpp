4ee0c6035092b979ab73c7ff5390a62ff88dd01d
torydebra <toridebraus@gmail.com>
2019-01-16 13:02:29 +0100

    average debugged

--- src/look_caresses_pkg/src/B_approach.cpp
+++ src/look_caresses_pkg/src/B_approach.cpp
@@ -7,7 +7,7 @@
 #include <numeric>
 #include <boost/circular_buffer.hpp>
 
-#define averageNum 10
+#define averageNum 20
 ros::Publisher pub;
 ros::Subscriber sub;
 boost::circular_buffer<float> sonarMsgs(averageNum);
@@ -16,19 +16,17 @@ boost::circular_buffer<float> sonarMsgs(averageNum);
 void sonarCallback(const sensor_msgs::Range &sensor_range)
 {
     look_caresses_pkg::platform_control msg;
-    sonarMsgs.push_back(sensor_range.range);
-    ROS_INFO("EOH %f", sonarMsgs.back());
+    sonarMsgs.push_front(sensor_range.range);
 
     if (sonarMsgs.full()){ //wait to have some values of range
 
-      float sum = std::accumulate(sonarMsgs.front(), sonarMsgs.back(), 0); //0 init the sum to 0
+      float sum = std::accumulate(sonarMsgs.begin(), sonarMsgs.end(), 0.0); //0 init the sum to 0
       float average = sum/averageNum;
-      ROS_INFO("Sonar Range: %f", sum);
+      ROS_INFO("Sonar Range average: %f", average);
 
       if (average > 0.15) { // in meters
-        ROS_INFO("GAY");
         msg.body_vel.linear.x = 30;
-        msg.body_vel.angular.z = 0.1; //because miro turns unwanted
+        msg.body_vel.angular.z = 0.05; //because miro turns unwanted
       }
       else {
         msg.body_vel.linear.x = 0;
