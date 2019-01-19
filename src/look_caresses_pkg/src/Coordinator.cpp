2d58f593f9e0952d54ad59b441f376cfd3347319
torydebra <toridebraus@gmail.com>
2019-01-19 18:38:46 +0100

    Coord finished

--- src/look_caresses_pkg/src/Coordinator.cpp
+++ src/look_caresses_pkg/src/Coordinator.cpp
@@ -1,11 +1,36 @@
 #include <ros/ros.h>
 #include "../header/A_awakening.h"
+#include "../header/Aa_faceDetection.h"
+#include "../header/B_approach.h"
+#include "../header/C_interaction.h"
+
 
 int main(int argc, char **argv)
 {
-
+  ROS_INFO("[COORD] Started");
+  ros::init(argc, argv, "Coordinator");
+  ros::NodeHandle nh;
+  ros::Publisher pubLoneliness = nh.advertise<std_msgs::Int32>("miro/look4caresses/loneliness", 1000);
+  pubLoneliness.publish(50); // initialize loneliness to 50
+  ROS_INFO("[COORD] Published initial value of loneliness = 50");
   A_awakening a_awakening(argc, argv);
-  a_awakening.main();
+  Aa_faceDetection aa_faceDetection(argc, argv);
+  B_approach b_approach(argc, argv);
+  C_interaction c_interaction(argc, argv);
 
-  ROS_INFO("Hello world!");
+  while (ros::ok()){
+    A_awakening a_awakening(argc, argv);
+    ROS_INFO("[COORD] node A started");
+    a_awakening.main();
+    ROS_INFO("[COORD] node A finished, Starting node Aa");
+    aa_faceDetection.main();
+    ROS_INFO("[COORD] node Aa finished, Starting node B");
+    b_approach.main();
+    ROS_INFO("[COORD] node B finished, Starting node C");
+    c_interaction.main();
+    ROS_INFO("[COORD] node C finished, Starting node A");
+  }
+
+  ROS_INFO("[COORD] Shutting down");
+  return 0;
 }
